f405364a9b8f376804173b300fafcb1223bc6962
singhshaurya28 <singhshaurya.2828@gmail.com>
2026-06-20 15:56:41 +0530

    Time: 11 ms (91.25%), Space: 12.6 MB (77.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,16 @@
 class Solution(object):
     def isPalindrome(self, s):
-        filtered = []
-
-        for ch in s:
-            # check a-z
-            if 'a' <= ch <= 'z':
-                filtered.append(ch)
-            # check A-Z (convert manually to lowercase)
-            elif 'A' <= ch <= 'Z':
-                filtered.append(chr(ord(ch) + 32))
-            # check 0-9
-            elif '0' <= ch <= '9':
-                filtered.append(ch)
-
-        i = 0
-        j = len(filtered) - 1
+        i, j = 0, len(s) - 1
 
         while i < j:
-            if filtered[i] != filtered[j]:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
                 return False
+
             i += 1
             j -= 1
 
